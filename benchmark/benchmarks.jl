91c8b51cfb30de92aba38636ba731cd33ed91a82
Marcelo Forets <mforets@gmail.com>
2019-05-27 11:29:19 -0300

    Update benchmarks.jl

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,9 +1,5 @@
 using BenchmarkTools, TaylorModels
 
-@static if VERSION > v"0.7.0"
-    using LinearAlgebra, SparseArrays
-end
-
 SUITE = BenchmarkGroup()  # parent BenchmarkGroup to contain our suite
 
 include("daisy.jl")
